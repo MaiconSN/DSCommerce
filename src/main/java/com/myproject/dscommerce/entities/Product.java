edf25fbf2d025c0524b1a05ce0950f81ca58c270
MaiconSN <maicon.joey@gmail.com>
2025-06-19 11:52:12 -0300

    Created Classes OrderItem/OrderItemPK for PrimaryKey composed

--- src/main/java/com/myproject/dscommerce/entities/Product.java
+++ src/main/java/com/myproject/dscommerce/entities/Product.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,20 +23,21 @@ public class Product {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@Column(columnDefinition = "TEXT")
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
+
 	@ManyToMany
-	@JoinTable(name = "tb_product_category",
-	joinColumns = @JoinColumn(name = "product_id"),
-	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
+
 	public Product() {
-		
+
 	}
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
@@ -108,6 +110,5 @@ public class Product {
 				&& Objects.equals(imgUrl, other.imgUrl) && Objects.equals(name, other.name)
 				&& Objects.equals(price, other.price);
 	}
-	
-	
+
 }
