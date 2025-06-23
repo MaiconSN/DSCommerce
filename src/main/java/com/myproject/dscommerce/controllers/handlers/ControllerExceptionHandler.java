18d7a8e031844c11bc622bb59fc5f8e56b1dd8e8
MaiconSN <maicon.joey@gmail.com>
2025-06-23 12:38:42 -0300

    Add Bean Validation

--- src/main/java/com/myproject/dscommerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/myproject/dscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -4,10 +4,13 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.myproject.dscommerce.dto.CustomError;
+import com.myproject.dscommerce.dto.ValidationError;
 import com.myproject.dscommerce.services.exceptions.DataBaseException;
 import com.myproject.dscommerce.services.exceptions.ResourceNotFoundException;
 
@@ -30,4 +33,14 @@ public class ControllerExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	}
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados Inválidos", request.getRequestURI());
+				
+		for(FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+		return ResponseEntity.status(status).body(err);
+	}
 }
