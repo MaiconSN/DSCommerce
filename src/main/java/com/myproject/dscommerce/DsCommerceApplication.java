a51c07f98cd87ba05a95e1afd1b341c903c4865f
MaiconSN <maicon.joey@gmail.com>
2025-07-03 18:23:31 -0300

    Implement Component BCrypt Password

--- src/main/java/com/myproject/dscommerce/DsCommerceApplication.java
+++ src/main/java/com/myproject/dscommerce/DsCommerceApplication.java
@@ -1,14 +1,23 @@
 package com.myproject.dscommerce;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DsCommerceApplication {
+public class DsCommerceApplication implements CommandLineRunner{
 	
+	/*@Autowired
+	private PasswordEncoder passwordEncoder;*/
 	
     public static void main(String[] args) {
         SpringApplication.run(DsCommerceApplication.class, args);
         
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("Encode: " + passwordEncoder.encode("123456"));
+		
+	}
 }
