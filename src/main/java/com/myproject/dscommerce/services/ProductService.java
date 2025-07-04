dfde7e1b0841acc21acc5ceb4aa4c7802f764375
MaiconSN <maicon.joey@gmail.com>
2025-07-04 20:20:47 -0300

    ProductMinDTO

--- src/main/java/com/myproject/dscommerce/services/ProductService.java
+++ src/main/java/com/myproject/dscommerce/services/ProductService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.myproject.dscommerce.dto.ProductDTO;
+import com.myproject.dscommerce.dto.ProductMinDTO;
 import com.myproject.dscommerce.entities.Product;
 import com.myproject.dscommerce.repositories.ProductRepository;
 import com.myproject.dscommerce.services.exceptions.DataBaseException;
@@ -31,11 +32,11 @@ public class ProductService {
 	}
 
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAll(String name, Pageable pageable) {
+	public Page<ProductMinDTO> findAll(String name, Pageable pageable) {
 		
 		Page<Product> result = repository.searchByName(name, pageable);
 		
-		return result.map(x -> new ProductDTO(x));
+		return result.map(x -> new ProductMinDTO(x));
 	}
 	
 	@Transactional
